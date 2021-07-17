57ac3a69f05032e77b19a0ffeed58d26ef0c5438
Aryan Chavan <aryan@aryansdomain.com>
2021-07-17 14:33:13 +0530

    add basic user registration

--- node-rest-api/routes/auth.js
+++ node-rest-api/routes/auth.js
@@ -1,7 +1,20 @@
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-	res.send("hey it's auth");
+const User = require("../models/User");
+
+// register
+router.post("/register", async (req, res) => {
+	const newUser = await new User({
+		username: req.body.username,
+		email: req.body.email,
+		password: req.body.password,
+	});
+	try {
+		const user = await newUser.save();
+		res.status(200).json(user);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 module.exports = router;
