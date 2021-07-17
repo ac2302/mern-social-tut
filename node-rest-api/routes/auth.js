93a4007e5b594dff3c29e0a7acbd3e0155bb9142
Aryan Chavan <aryan@aryansdomain.com>
2021-07-17 14:41:05 +0530

    add password hashing

--- node-rest-api/routes/auth.js
+++ node-rest-api/routes/auth.js
@@ -1,17 +1,25 @@
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
 
 const User = require("../models/User");
 
 // register
 router.post("/register", async (req, res) => {
-	const newUser = await new User({
-		username: req.body.username,
-		email: req.body.email,
-		password: req.body.password,
-	});
 	try {
-		const user = await newUser.save();
-		res.status(200).json(user);
+		// hashing password
+		const salt = await bcrypt.genSalt(10);
+		const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+		// creating new user
+		const newUser = await new User({
+			username: req.body.username,
+			email: req.body.email,
+			password: hashedPassword,
+		});
+
+		// save user
+		const savedUser = await newUser.save();
+		res.status(200).json(savedUser);
 	} catch (err) {
 		res.status(500).json(err);
 	}
