2ff83331d964214deb558efb8ef0d609a8381d62
Aryan Chavan <aryan@aryansdomain.com>
2021-07-17 15:04:21 +0530

    add login

--- node-rest-api/routes/auth.js
+++ node-rest-api/routes/auth.js
@@ -21,7 +21,33 @@ router.post("/register", async (req, res) => {
 		const savedUser = await newUser.save();
 		res.status(200).json(savedUser);
 	} catch (err) {
-		res.status(500).json(err);
+		res.status(500).json({ err });
+	}
+});
+
+// login
+router.post("/login", async (req, res) => {
+	try {
+		const user = await User.findOne({ email: req.body.email });
+
+		if (!user) {
+			res.status(400).json({ message: "incorrect username or password" });
+			return;
+		}
+
+		const isValidPassword = await bcrypt.compare(
+			req.body.password,
+			user.password
+		);
+
+		if (!isValidPassword) {
+			res.status(400).json({ message: "incorrect username or password" });
+			return;
+		}
+
+		res.status(200).json({ loggedIn: true, user });
+	} catch (err) {
+		res.status(500).json({ err });
 	}
 });
 
